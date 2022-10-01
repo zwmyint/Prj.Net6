503e67c775a4e18b5b52d5cf9d706fec11957224
ZAW WIN MYINT <45326766+zwmyint@users.noreply.github.com>
2022-10-02 03:07:50 +0800

    20221001 modified controller

--- Prj.Net6.API/Controllers/WeatherForecastController.cs
+++ Prj.Net6.API/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Prj.Net6.API.Controllers
 {
@@ -11,23 +12,29 @@ namespace Prj.Net6.API.Controllers
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<WeatherForecastController> logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
-            _logger = logger;
+            this.logger = logger;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            logger.LogDebug("Inside GetWeatherForecast endpoint");
+
+            //throw new Exception("Failed to retrieve data");
+            var response = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            }).ToArray();
+
+            logger.LogDebug($"The response for the get weather forecast is {JsonConvert.SerializeObject(response)}");
+
+            return response;
         }
     }
 }
